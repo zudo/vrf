77d4379946fa0408955c14d6b76bc089922c05de
zudo <44712315+zudo@users.noreply.github.com>
2023-06-16 02:25:35 +0200

    update

--- src/lib.rs
+++ src/lib.rs
@@ -96,55 +96,54 @@ impl VRF {
             .finalize()
     }
 }
+pub struct As(Scalar);
 #[cfg(test)]
 mod tests {
     use super::*;
+    use lazy_static::lazy_static;
     use rand_core::OsRng;
     use sha2::Sha224;
     use sha2::Sha256;
     use sha2::Sha512;
+    pub const ALPHA_0: &[u8] = b"hello from";
+    pub const ALPHA_1: &str = "zudo";
+    lazy_static! {
+        static ref SECRET_0: Scalar = scalar_random(&mut OsRng);
+        static ref SECRET_1: Scalar = scalar_random(&mut OsRng);
+        static ref PUBLIC_0: RistrettoPoint = *SECRET_0 * G;
+        static ref PUBLIC_1: RistrettoPoint = *SECRET_1 * G;
+    }
     #[test]
     fn sign_verify() {
         let rng = &mut OsRng;
-        let secret = scalar_random(rng);
-        let public = secret * G;
-        let alpha = [0, 1, 2, 3];
-        let vrf_0 = VRF::sign::<Sha512, Sha256>(rng, &secret, &alpha);
-        let vrf_1 = VRF::sign::<Sha512, Sha256>(rng, &secret, &alpha);
+        let vrf_0 = VRF::sign::<Sha512, Sha256>(rng, &SECRET_0, &ALPHA_0);
+        let vrf_1 = VRF::sign::<Sha512, Sha256>(rng, &SECRET_0, &ALPHA_0);
         assert_eq!(vrf_0.gamma, vrf_1.gamma);
         assert_ne!(vrf_0.c, vrf_1.c);
         assert_ne!(vrf_0.s, vrf_1.s);
         let beta_0 = vrf_0.beta::<Sha224>();
         let beta_1 = vrf_1.beta::<Sha224>();
         assert_eq!(beta_0, beta_1);
-        assert!(vrf_0.verify::<Sha512, Sha256, Sha224>(&public, &alpha, &beta_0));
-        assert!(vrf_1.verify::<Sha512, Sha256, Sha224>(&public, &alpha, &beta_1));
+        assert!(vrf_0.verify::<Sha512, Sha256, Sha224>(&PUBLIC_0, &ALPHA_0, &beta_0));
+        assert!(vrf_1.verify::<Sha512, Sha256, Sha224>(&PUBLIC_0, &ALPHA_0, &beta_1));
     }
     #[test]
     fn sign_verify_fake() {
         let rng = &mut OsRng;
-        let secret = scalar_random(rng);
-        let public = secret * G;
-        let alpha = [0, 1, 2, 3];
-        let vrf = VRF::sign::<Sha512, Sha256>(rng, &secret, &alpha);
+        let vrf = VRF::sign::<Sha512, Sha256>(rng, &SECRET_0, &ALPHA_0);
         let beta = vrf.beta::<Sha224>();
-        let secret_fake = scalar_random(rng);
-        let public_fake = secret_fake * G;
-        let alpha_fake = [3, 2, 1, 0];
-        assert!(!vrf.verify::<Sha512, Sha256, Sha224>(&public_fake, &alpha, &beta));
-        assert!(!vrf.verify::<Sha512, Sha256, Sha224>(&public, &alpha_fake, &beta));
+        assert!(!vrf.verify::<Sha512, Sha256, Sha224>(&PUBLIC_1, &ALPHA_0, &beta));
+        assert!(!vrf.verify::<Sha512, Sha256, Sha224>(&PUBLIC_0, &ALPHA_1, &beta));
     }
     #[test]
     fn to_bytes_from_slice() {
         let rng = &mut OsRng;
-        let secret = scalar_random(rng);
-        let public = secret * G;
-        let vrf = VRF::sign::<Sha512, Sha256>(rng, &secret, &[]);
-        let secret_bytes = secret.to_bytes();
-        let public_bytes = public.compress().to_bytes();
+        let vrf = VRF::sign::<Sha512, Sha256>(rng, &SECRET_0, ALPHA_0);
+        let secret_bytes = SECRET_0.to_bytes();
+        let public_bytes = PUBLIC_0.compress().to_bytes();
         let vrf_bytes = vrf.to_bytes();
-        assert_eq!(secret, scalar_from_canonical(secret_bytes).unwrap());
-        assert_eq!(public, point_from_slice(&public_bytes).unwrap());
+        assert_eq!(*SECRET_0, scalar_from_canonical(secret_bytes).unwrap());
+        assert_eq!(*PUBLIC_0, point_from_slice(&public_bytes).unwrap());
         assert_eq!(vrf, VRF::from_slice(&vrf_bytes).unwrap());
     }
 }
